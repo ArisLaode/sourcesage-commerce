2fc13c80a21cc617370a1cf66f41e5a72b3dac5d
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 03:14:10 +0700

    add configuration url api

---
+++ fe-ecommerce/src/utils/Config.js
@@ -0,0 +1,10 @@
+const baseUrl = "http://127.0.0.1:8082"
+const createProduct = `${baseUrl}/api/v1/products/create`
+const readProduct = `${baseUrl}/api/v1/products/read`
+const readVariant = `${baseUrl}/api/v1/variants/read`
+
+export {
+    readProduct,
+    readVariant,
+    createProduct
+}
