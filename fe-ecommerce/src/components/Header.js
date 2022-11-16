c31e4bc0bf489827af2421dd5cad6bb509f2d8b2
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 06:32:13 +0700

    add menu add variant

--- fe-ecommerce/src/components/Header.js
+++ fe-ecommerce/src/components/Header.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import { Menu } from 'antd';
 import {
     HomeOutlined,
-    FolderAddOutlined
+    QrcodeOutlined,
+    ClusterOutlined
  } from '@ant-design/icons';
 
 const centerStyle = {
@@ -16,12 +17,15 @@ function HeaderApp (){
   return (
     <>
         <Menu mode="horizontal" theme='dark' style={centerStyle}>
-          <Menu.Item key="/" icon={<HomeOutlined />}>
-            <Link to='/' style={{paddingRight: '16px'}}>Home</Link>
-          </Menu.Item>
-          <Menu.Item key="/add-product" icon={<FolderAddOutlined />}>
-            <Link to='/add-product' style={{paddingRight: '16px'}}>Add Product</Link>
-          </Menu.Item>
+            <Menu.Item key="/" icon={<HomeOutlined />}>
+                <Link to='/' style={{paddingRight: '16px'}}>Home</Link>
+            </Menu.Item>
+            <Menu.Item key="/add-product" icon={<QrcodeOutlined />}>
+                <Link to='/add-product' style={{paddingRight: '16px'}}>Add Product</Link>
+            </Menu.Item>
+            <Menu.Item key="/add-variant" icon={<ClusterOutlined />}>
+                <Link to='/add-variant' style={{paddingRight: '16px'}}>Add Variant</Link>
+            </Menu.Item>
         </Menu>
     </>
   );
