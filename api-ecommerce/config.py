bba07283171bc7aac6720b3fc319f04135ed35d6
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 06:43:09 +0700

    add generate secret key flask

--- api-ecommerce/config.py
+++ api-ecommerce/config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.urandom(24)
     UPLOAD_FOLDER = os.path.join(basedir, 'static/images')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
