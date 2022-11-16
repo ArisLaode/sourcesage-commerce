e3e87a9c8285fa73a76a2e85375cab83b15b01b4
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 03:26:25 +0700

    add part docker host fe

--- fe-ecommerce/src/utils/Config.js
+++ fe-ecommerce/src/utils/Config.js
@@ -1,3 +1,4 @@
+// const baseUrl = "/service"
 const baseUrl = "http://127.0.0.1:8082"
 const createProduct = `${baseUrl}/api/v1/products/create`
 const readProduct = `${baseUrl}/api/v1/products/read`
