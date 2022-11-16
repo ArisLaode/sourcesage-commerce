48c2c7d89e6f6a6efa7032083b334e34153c0c4f
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 03:09:53 +0700

    add route fe

---
+++ fe-ecommerce/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import './App.css';
+import Index from "./pages/Index";
+import ProductDetail from "./pages/ProductDetail";
+import AddProduct from "./pages/AddProduct";
+
+const App = () => (
+      <>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/read/:productId" exact element={<ProductDetail />} />
+            <Route path="/add-product" exact element={<AddProduct />} />
+        </Routes>
+      </Router>
+      </>
+);
+
+
+export default App;
