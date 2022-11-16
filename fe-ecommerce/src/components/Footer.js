9612c563d53b49f36a52db244187f2c97f7bea34
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 03:12:19 +0700

    add footer fe

---
+++ fe-ecommerce/src/components/Footer.js
@@ -0,0 +1,13 @@
+import {Layout} from 'antd';
+
+const { Footer } = Layout;
+
+function FooterApp() {
+  return (
+      <>
+          <Footer style={{ textAlign: 'center' }}>Commerce ©2022 Created by Dev</Footer>
+      </>
+  );
+}
+
+export default FooterApp;
