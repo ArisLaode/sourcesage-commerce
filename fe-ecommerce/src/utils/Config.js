a3a217f278a2d9e9d88a5ab2414308c7b9fbea06
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 05:34:57 +0700

    add config host:port via local and docker

--- fe-ecommerce/src/utils/Config.js
+++ fe-ecommerce/src/utils/Config.js
@@ -1,5 +1,9 @@
-// const baseUrl = "/service"
-const baseUrl = "http://127.0.0.1:8082"
+
+// host & port via docker
+const baseUrl = "/service"
+
+// host & port via local
+// const baseUrl = "http://127.0.0.1:8082"
 const createProduct = `${baseUrl}/api/v1/products/create`
 const readProduct = `${baseUrl}/api/v1/products/read`
 const readVariant = `${baseUrl}/api/v1/variants/read`
