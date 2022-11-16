d1f1e1cc2060a9e4c439470a309e88cb6b32ba19
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-17 02:20:06 +0700

    model product

---
+++ api-ecommerce/app/models/product.py
@@ -0,0 +1,17 @@
+
+from datetime import datetime
+from extensions import db
+
+
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text(), nullable=True)
+    images = db.Column(db.String(255), nullable=False)
+    logo_id = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"product : {self.name}"
